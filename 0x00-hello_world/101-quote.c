8f78a3e5a64e10c83515162f9834f8bf959a1aed
glaudinekorrie <chepkorirglaudine3@gmail.com>
2022-12-23 13:27:40 -0800

    Programme C

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<unistd.h>
+/**
+ * main function is void
+ * return value must be 1 and not 1
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	retun (1);
+}
