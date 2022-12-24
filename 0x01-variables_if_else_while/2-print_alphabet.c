0173a0cbf152941f2c23a0823289c44aa6e3896c
glaudinekorrie <chepkorirglaudine3@gmail.com>
2022-12-24 13:17:58 -0800

    Programming

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - Execution starts
+ *
+ * Return: Value 0(Success)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+
+	return (0);
+}
+
