6cf3a4bc47a714ffe896649c0f570a427bda2c4b
glaudinekorrie <chepkorirglaudine3@gmail.com>
2022-12-24 13:32:56 -0800

    Programming

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - Excecution starts
+ *
+ * Return- value is 0 (Success)
+ */
+int main(void)
+{
+	char letters;
+
+	for (letters = 'a'; letters <= 'z'; letters++)
+	{
+		putchar(letters);
+	}
+	for (letters = 'A'; letters <= 'Z'; letters++)
+	{
+		putchar(letters);
+	}
+	putchar('\n');
+
+	return (0);
+}
