8615235eeb67c5899c6f04a5530ee4b445955466
glaudinekorrie <chepkorirglaudine3@gmail.com>
2022-12-25 09:56:58 -0800

    Programming

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main -Execution starts
+ *
+ * Return: value is 0 (Success)
+ */
+int main(void)
+{
+	int arr[i, j];
+
+	for(i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j<= '9'; j++)
+		{
+			putchar(arr[i ,j]);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
