958bfab3f1ebcbb4e12c8f6bfeeac2718ce8bf1b
glaudinekorrie <chepkorirglaudine3@gmail.com>
2022-12-25 08:13:40 -0800

    Programming

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main-Execution starts
+ *
+ * Return: value is 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 15; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
