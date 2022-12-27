4bc8937e5c7582dc6f0ce7c7767f856f56724503
glaudinekorrie <chepkorirglaudine3@gmail.com>
2022-12-27 07:12:37 -0800

    Programming

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main -execuiton tarts
+ *
+ * Return: value is 0(Success)
+ */
+int main(void)
+{
+	int i, j, max;
+	for (i = 0; i < max; i++)
+	{
+		for (j =i + 1; j = max; j++)
+		{
+			putchar(i/10 +'0');
+			putchar(j % 10 + '0');
+			putchar(' ');
+			putchar(j/10 + '0');
+			putchar(j % 10 + '0');
+			if (i != 98 && j != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
