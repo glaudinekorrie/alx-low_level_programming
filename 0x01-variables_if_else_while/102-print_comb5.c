f8a6d2fcea5799507c463a9f7884705ab5122bbd
glaudinekorrie <chepkorirglaudine3@gmail.com>
2022-12-27 07:21:34 -0800

    Programming

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,15 +9,18 @@ int main(void)
 {
 	int i, j;
 	int max = 100;
+
 	for (i = 0; i < max; i++)
 	{
-		for (j =i + 1; j < max; j++)
+		for (j = i + 1; j < max; j++)
 		{
-			putchar(i/10 +'0');
+			putchar(i /10 +'0');
 			putchar(j % 10 + '0');
 			putchar(' ');
-			putchar(j/10 + '0');
+
+			putchar(j /10 + '0');
 			putchar(j % 10 + '0');
+
 			if (i != 98 && j != 99)
 			{
 				putchar(',');
