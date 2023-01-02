e17d7c1307810476feff1c2c78ebec2f5f594b5f
glaudinekorrie <chepkorirglaudine3@gmail.com>
2023-01-02 12:51:22 -0800

    loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "main.h"
+/**
+ * _abs - to be executed
+ *
+ * Return: value is 0
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+
+	return (-n);
+}
