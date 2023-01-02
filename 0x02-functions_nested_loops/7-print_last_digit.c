ce6b540318e70b9c2136c80e00dc260d91880140
glaudinekorrie <chepkorirglaudine3@gmail.com>
2023-01-02 13:01:38 -0800

    loops

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "main.h"
+/**
+ * print_last_digit - to be executed
+ *
+ * Return: value is 0
+ */
+int print_last_digit(int r)
+{
+	int last;
+
+	last = r % 10;
+	return (last);
+}
