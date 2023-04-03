ed5b60cae3e7268a041ef02dc724df7a7b8d388e
glaudinekorrie <chepkorirglaudine3@gmail.com>
2023-04-03 08:59:41 -0700

    Loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ * add - Adds two digits
+ *
+ * @n: to be checked
+ *
+ * Return: Value is result of addition
+ */
+int add(int, int)
+{
+	int n, i, j;
+
+	n = i + j;
+
+	return (n);
+}
