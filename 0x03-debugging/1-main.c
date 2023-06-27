d2dd4a3638e1f0a522effa8f67feef781d1ad917
glaudinekorrie <chepkorirglaudine3@gmail.com>
2023-06-27 00:46:13 -0700

    debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,9 +7,9 @@
 
 int main(void)
 {
-    int i;
+	int i;
 
-    printf("Infinite loop incoming :(\n");
+	printf("Infinite loop incoming :(\n");
 
     /**
      *i = 0;
@@ -19,8 +19,8 @@ int main(void)
      *}
      */
 
-    printf("Infinite loop avoided! \\o/\n");
+	printf("Infinite loop avoided! \\o/\n");
 
-    return (0);
+	return (0);
 }
 
