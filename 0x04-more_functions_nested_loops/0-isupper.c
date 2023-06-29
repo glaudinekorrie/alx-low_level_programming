03f0818ddae0f869b6801bab0eb3e26b21f196f2
glaudinekorrie <chepkorirglaudine3@gmail.com>
2023-06-29 02:47:43 -0700

    loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include<stdio.h>
+
+
+/**
+ * _isupper -  checks for uppercase character.
+ * @c: c -  Variable
+ * Return: Always 0.
+ */
+
+int _isupper(int c)
+{
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		return (1);
+	}
+	return (0);
+}
