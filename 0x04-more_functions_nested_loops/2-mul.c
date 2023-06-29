6df6afaa44becd136cc4c5fe75ef9d03e74b1806
glaudinekorrie <chepkorirglaudine3@gmail.com>
2023-06-29 03:27:19 -0700

    loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * mul - execution starts
+ * @a - value to be read
+ * @b - value to be read
+ * return : velue is the multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
