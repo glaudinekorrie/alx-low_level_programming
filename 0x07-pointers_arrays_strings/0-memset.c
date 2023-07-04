2b2d344ce7071f1da4827172eff8a0567f48fe57
glaudinekorrie <chepkorirglaudine3@gmail.com>
2023-07-04 00:44:16 -0700

    pointers

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,16 +2,17 @@
 
 /**
  * _memset()- function to be executed
- * @*s - pointer
- * @b - variable
- * @n - amount of space
- * return : always 0
+ * @s : pointer
+ * @b : variable
+ * @n : amount of space
+ *
+ * Return: char with result of memset
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
-	
+
 	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
